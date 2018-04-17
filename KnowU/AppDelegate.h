34d9b5bdb2b744f174a4c18def9389fba45295ca
young He <younghe@youngdeiMac.local>
2018-04-17 18:41:48 +0800

    EmptyProject

---
+++ KnowU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KnowU
+//
+//  Created by young He on 2018/4/17.
+//  Copyright © 2018年 Hy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
